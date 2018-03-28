e761dbc515688af880e00ba4b986961e8f3de150
sangyong han <syhan@cloudrim.co.kr>
2018-03-28 19:35:55 +0900

    레이아웃 작업중...

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -4,8 +4,8 @@ import React, {Component} from 'react';
 class Dashboard extends Component {
     render() {
         return (
-          <div className="gr-dashboard">
-              gr-dashboard
+          <div className="dashboard">
+              dashboard
           </div>
     
         )
