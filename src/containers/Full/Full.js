7ba6562ab0d8c3fbf82f24c61afd339c62823e6c
sangyong han <syhan@cloudrim.co.kr>
2018-04-03 19:09:05 +0900

    레이아웃 디자인 헤더와 사이드 완료, 본문 디자인 필요

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -1,42 +1,86 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { withStyles } from "material-ui/styles";
+import Typography from "material-ui/Typography";
+
+import { grLayout } from "../../templates/default/GrLayout";
+import { grColor } from "../../templates/default/GrColors";
 
 import { Switch, Route, Redirect } from "react-router-dom";
-import classNames from "classnames";
 
 import GrHeader from "../Header/";
 import GrSideMenu from "../GrSideMenu";
+import GrBreadcrumb from "../Breadcrumb/";
+
 import Button from "material-ui/Button";
 
 import Sidebar from "../Sidebar/";
-import Breadcrumb from "../Breadcrumb/";
+
 import Container from "../Container/";
 import Aside from "../Aside/";
 import Footer from "../Footer/";
 import Dashboard from "../Dashboard/";
 
+
 const styles = {
+  root: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column"
+  },
   appBody: {
-    marginTop: 55,
+    marginTop: grLayout.headerHeight,
+    display: "flex",
+    flexDirection: "row",
+    flexGrow: 1,
+    overflowX: "hidden",
     background: 'linear-gradient(45deg, #feb26b 30%, #ffd753 90%)',
-    zIndex: 2100
+  },
+  main: {
+    marginRight: 0,
+    marginLeft: grLayout.sideBarWidth,
+    flex: 1,
+    zIndex: 1200,
+    minWidth: 0,
+    transition: "margin-left 0.25s, margin-right 0.25s, width 0.25s, flex 0.25s",
+    display: "block",
+  },
+  mainWide: {
+    marginRight: 0,
+    marginLeft: 0,
+    flex: 1,
+    zIndex: 1200,
+    minWidth: 0,
+    transition: "margin-left 0.25s, margin-right 0.25s, width 0.25s, flex 0.25s",
+    display: "block",
   }
+
 };
 
 class Full extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sideOpen: true
+      sideOpen: true,
+      isMainWide: false
     };
 
     this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.changeTempValue = this.changeTempValue.bind(this);
+  }
+
+  changeTempValue() {
+    console.log(this.state.tempValue);
+    this.setState({
+      tempValue: "zzz"
+    });
   }
 
   toggleDrawer() {
     this.setState({
-      sideOpen: !this.state.sideOpen
+      sideOpen: !this.state.sideOpen,
+      isMainWide: !this.state.isMainWide,
     });
   }
 
@@ -45,14 +89,12 @@ class Full extends Component {
     const { classes } = this.props;
 
     return (
-      <div>
+      <div className={classes.root} >
         <GrHeader toggleDrawer={this.toggleDrawer} />
         <div className={classes.appBody}>
-          
           <GrSideMenu sideOpen={this.state.sideOpen} />
-          <main className="main">
-
-            <Breadcrumb />
+          <main className={classNames({[classes.main]: !this.state.isMainWide}, {[classes.mainWide]: this.state.isMainWide})}>
+            <GrBreadcrumb />
             <Container fluid>
               <Switch>
                 <Route
@@ -75,4 +117,5 @@ Full.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 export default withStyles(styles)(Full);
+
 
---
+++ src/templates/default/GrLayout.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export const grLayout = {
+
+  headerHeight: "55px",
+
+  sideBarWidth: "240px"
+  
+}
