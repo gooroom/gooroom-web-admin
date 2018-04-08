96190aaaa26833f4eedaf2d21d01473b5986e501
sangyong han <syhan@cloudrim.co.kr>
2018-04-09 01:28:24 +0900

    컨텐트 개발중

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -26,22 +26,14 @@ import blue from 'material-ui/colors/blue';
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
   },
   overrides: {
-    MuiButton: {
-      // Name of the styleSheet
+    MuiPaper: {
       root: {
-        // Name of the rule
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        borderRadius: 3,
-        border: 0,
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
-      },
-    },
+        background: "#E0E0E0",
+        color: "#3c50c1",
+      }
+    }
   },
 });
 
