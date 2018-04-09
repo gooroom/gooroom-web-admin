f2846d4d6efd65258070210546eceb80d43f2c28
sangyong han <syhan@gooroom.kr>
2018-04-10 02:06:31 +0900

    POST 파라미터 처리중. 테스트 필요

--- src/views/Clients/ClientManage.js
+++ src/views/Clients/ClientManage.js
@@ -27,33 +27,32 @@ import { MenuItem } from "material-ui/Menu";
 import Input, { InputLabel } from "material-ui/Input";
 import { FormControl, FormHelperText } from "material-ui/Form";
 
+import Button from "material-ui/Button";
+import Search from "@material-ui/icons/Search";
 
-import Button from 'material-ui/Button';
-import Search from '@material-ui/icons/Search';
-
-const styles= theme => ({
+const styles = theme => ({
   content: {
-    height: "100%",
+    height: "100%"
   },
   pageContent: {
-    paddingTop: 2,
+    paddingTop: 2
   },
   control: {
     marginTop: 0
   },
   formControl: {
     minWidth: 120,
-    marginRight: "15px",
+    marginRight: "15px"
   },
   form: {
     marginBottom: 15
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   leftIcon: {
-    marginRight: theme.spacing.unit,
-  },
+    marginRight: theme.spacing.unit
+  }
 });
 
 class ClientManage extends Component {
@@ -74,56 +73,37 @@ class ClientManage extends Component {
         { id: "ALL", value: "전체", label: "전체" }
       ],
       keyword: "",
-      gridData: [],
+      gridData: []
     };
 
     this.fetchData = this.fetchData.bind(this);
   }
 
-  componentDidMount() {
-    var that = this;
-    axios({
-      method: "post",
-      url: "http://localhost:8080/gpms/readClientGroupList",
-      config: { headers: { "Content-Type": "multipart/form-data" } },
-      data: {}
-    })
-      .then(function(response) {
-        if (response.data.data && response.data.data.length > 0) {
-          const groupList = response.data.data.map(x => ({
-            key: x.grpId,
-            id: x.grpId,
-            value: x.grpNm,
-            label: x.grpNm,
-          }));
-          that.setState({ clientGroupOptionList: groupList });
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-
-    // axios.post('http://localhost:8080/gpms/readClientGroupList', {
-    //   firstName: 'Fred',
-    //   lastName: 'Flintstone'
-    // })
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-
+  getClientData() {
     // select data
     var bodyFormData = new FormData();
     bodyFormData.set("start", "0");
     bodyFormData.set("length", "5");
-    var that2 = this;
+    var that = this;
+
     axios({
       method: "post",
       url: "http://localhost:8080/gpms/readClientList",
-      config: { headers: { "Content-Type": "multipart/form-data" } },
-      data: bodyFormData
+      headers: { "Content-Type": "application/json" },
+      transformRequest: [function (data, headers) {
+        // Do whatever you want to transform the data
+        const params = new FormData ();
+        params.set("keyword", that.state.keyword);
+
+    
+        return data;
+      }],
+      data: {
+        start: "0",
+        length: "5",
+        keyword: that.state.keyword,
+        clientStatus: that.state.clientStatus,
+      }
     })
       .then(function(response) {
         if (response.data.data && response.data.data.length > 0) {
@@ -133,12 +113,118 @@ class ClientManage extends Component {
             value: x.grpNm
           }));
           const gridList2 = response.data.data;
-          that2.setState({ gridData: gridList2 });
+          that.setState({ gridData: gridList2 });
         }
       })
       .catch(function(error) {
         console.log(error);
       });
+  }
+
+  componentDidMount() {
+
+    
+
+
+
+    axios.post('/gpms/testSite', {
+      param1: 'param1',
+      param2: 'param2'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+/*
+    axios.post('http://localhost:9191/gpms/readClientGroupList', {
+      param1: 'param1',
+      param2: 'param2'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+    
+
+
+    var that = this;
+
+    var params = new FormData();
+    params.set("param1", "param1");
+    params.set("param2", "param2");
+
+    axios({
+      method: "post",
+      url: "http://localhost:8080/gpms/readClientGroupList",
+      config: { headers: { "Content-Type": "multipart/form-data" } },
+      data: params,
+      withCredentials: true
+    })
+      .then(function(response) {
+        if (response.data.data && response.data.data.length > 0) {
+          const groupList = response.data.data.map(x => ({
+            key: x.grpId,
+            id: x.grpId,
+            value: x.grpNm,
+            label: x.grpNm
+          }));
+          that.setState({ clientGroupOptionList: groupList });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+*/
+
+
+      var that = this;
+
+      var params = new FormData();
+      params.set("param1", "param1");
+      params.set("param2", "param2");
+  
+      axios({
+        method: "post",
+        url: "http://localhost:8080/gpms/readClientGroupList",
+        transformRequest: [function (data, headers) {
+          // Do whatever you want to transform the data
+      
+          return "aa=aaa&bb=bbb";
+        }],
+        data: {
+          param3: "param3",
+          param4: "param4",
+          param5: "param5",
+        },
+        withCredentials: true
+      })
+        .then(function(response) {
+          if (response.data.data && response.data.data.length > 0) {
+            const groupList = response.data.data.map(x => ({
+              key: x.grpId,
+              id: x.grpId,
+              value: x.grpNm,
+              label: x.grpNm
+            }));
+            that.setState({ clientGroupOptionList: groupList });
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+  
+  
+  
+
+
+    this.getClientData();
   }
 
   fetchData(state, instance) {
@@ -162,13 +248,12 @@ class ClientManage extends Component {
     //console.log("### : "  + event);
     this.setState({ [event.target.name]: event.target.value });
   };
-  
+
   handleChangeKeyword = name => event => {
     this.setState({
-      [name]: event.target.value,
+      [name]: event.target.value
     });
   };
-
 
   render() {
     //console.log("-ClientManage.render-------------------------");
@@ -181,7 +266,6 @@ class ClientManage extends Component {
           <GrPageHeader path={this.props.location.pathname} />
           <CardContent className={classes.pageContent}>
             <form className={classes.form}>
-              
               <FormControl className={classes.formControl} autoComplete="off">
                 <InputLabel htmlFor="client-status">단말상태</InputLabel>
                 <Select
@@ -211,23 +295,29 @@ class ClientManage extends Component {
                   ))}
                 </Select>
               </FormControl>
-              
+
               <FormControl className={classes.formControl} autoComplete="off">
                 <TextField
                   id="keyword"
                   label="검색어"
                   className={classes.textField}
                   value={this.state.keyword}
-                  onChange={this.handleChangeKeyword('keyword')}
+                  onChange={this.handleChangeKeyword("keyword")}
                   margin="normal"
                 />
               </FormControl>
 
-              <Button className={classes.button} variant="raised" color="primary">
+              <Button
+                className={classes.button}
+                variant="raised"
+                color="primary"
+                onClick={() => {
+                  this.getClientData();
+                }}
+              >
                 <Search className={classes.leftIcon} />
                 조회
               </Button>
-
             </form>
 
             <ReactTable
