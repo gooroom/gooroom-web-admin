4e2ae5f9d23ecb3a51f086dd43d05c9388e57ef4
sangyong han <syhan@cloudrim.co.kr>
2018-04-11 02:17:06 +0900

    테이블 라이브러리를 material-ui 용으로 변경 - 진행중

--- src/views/Clients/ClientManage.js
+++ src/views/Clients/ClientManage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import classNames from 'classnames';
 import { withStyles } from "material-ui/styles";
 
 import { grLayout } from "../../templates/default/GrLayout";
@@ -11,6 +12,17 @@ import Card, {
   CardContent,
   CardActions
 } from "material-ui/Card";
+
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+} from 'material-ui/Table';
+import Checkbox from 'material-ui/Checkbox';
+import Tooltip from 'material-ui/Tooltip';
 
 import GrOptions from "../../containers/GrOptions";
 import GrPageHeader from "../../components/GrHeader/GrPageHeader";
@@ -49,90 +61,140 @@ const styles = theme => ({
     marginBottom: 15
   },
 
+  table: {
+    minWidth: 700,
+  },
+
   button: {
     margin: theme.spacing.unit
   },
   leftIcon: {
     marginRight: theme.spacing.unit
-  }
+  },
+
+  headCell: {
+    whiteSpace: "nowrap"
+  },
+  tableContainer: {
+    overflowX: "auto"
+  },
 });
 
 
-const requestData = (pageSize, page, sorted, filtered) => {
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
 
+const columnData = [
+  { id: 'ischeck', numeric: false, disablePadding: true, label: '선택' },
+  { id: 'clientStatus', numeric: true, disablePadding: false, label: '상태' },
+  { id: 'clientId', numeric: true, disablePadding: false, label: '단말아이디' },
+  { id: 'clientName', numeric: true, disablePadding: false, label: '단말이름' },
+  { id: 'loginId', numeric: true, disablePadding: false, label: '접속자' },
+  { id: 'clientGroupName', numeric: true, disablePadding: false, label: '단말그룹' },
+  { id: 'regDate', numeric: true, disablePadding: false, label: '등록일' },
+];
 
+const data = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 24, 4.0),
+  createData('Eclair', 262, 16.0, 24, 6.0, 24, 4.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 24, 4.0),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 24, 4.0),
+];
 
-  return new Promise((resolve, reject) => {
+class ClientManageHead extends Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
 
+  render() {
+    const { classes } = this.props;
 
-    console.log("pageSize : " + pageSize);
-    console.log("page : " + page);
-    console.log("sorted : " + sorted);
-    console.log("filtered : " + filtered);
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
 
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox" className={classes.headCell}>
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={numSelected === rowCount}
+              onChange={onSelectAllClick}
+            />
+          </TableCell>
+          {columnData.map(column => {
+            return (
+              <TableCell
+                className={classes.headCell}
+                key={column.id}
+                numeric={column.numeric}
+                padding={column.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === column.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={order}
+                    onClick={this.createSortHandler(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
 
-    //var that = this;
-    let rawData = [];
-    axios({
-      method: "post",
-      url: "http://localhost:8080/gpms/readClientList",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      transformRequest: [function (data, headers) {
-        return qs.stringify(data);
-      }],
-      data: {
-        start: "0",
-        length: "5",
-        searchKey: that.state.keyword,
-        clientType: that.state.clientStatus,
-        groupId: that.state.clientGroup,
-      },
-      withCredentials: true
-    }).then(function(response) {
-
-        if (response.data.data && response.data.data.length > 0) {
-          const gridList = response.data.data.map(x => ({
-            key: x.grpId,
-            id: x.grpId,
-            value: x.grpNm
-          }));
-
-          rawData = response.data.data;
-
-          //const gridList2 = response.data.data;
-          //that.setState({ clientListData: gridList2 });
-        }
-
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-
-
-
-
-
-    // You can retrieve your data however you want, in this case, we will just use some local data.
-    let filteredData = rawData;
-
-    // You must return an object containing the rows of the current page, and optionally the total pages number.
-    const res = {
-      rows: filteredData.slice(pageSize * page, pageSize * page + pageSize),
-      pages: Math.ceil(filteredData.length / pageSize)
-    };
-
-    // Here we'll simulate a server response with 500ms of delay.
-    setTimeout(() => resolve(res), 500);
-  });
+ClientManageHead.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
+
+ClientManageHead = withStyles(styles)(ClientManageHead);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class ClientManage extends Component {
 
+  
   constructor(props) {
     super(props);
 
     this.state = {
+
       collapse: true,
       loading: true,
 
@@ -148,7 +210,30 @@ class ClientManage extends Component {
       ],
       keyword: "",
 
-      clientListData: []
+      clientListData: [],
+
+
+
+      order: 'asc',
+      orderBy: 'calories',
+      selected: [],
+      data: [
+        createData('Cupcake', 305, 3.7, 67, 4.3, 67, 4.3),
+        createData('Donut', 452, 25.0, 51, 4.9, 67, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0, 67, 4.3),
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9, 67, 4.3),
+        createData('Honeycomb', 408, 3.2, 87, 6.5, 67, 4.3),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 67, 4.3),
+        createData('Jelly Bean', 375, 0.0, 94, 0.0, 67, 4.3),
+        createData('KitKat', 518, 26.0, 65, 7.0, 67, 4.3),
+        createData('Lollipop', 392, 0.2, 98, 0.0, 67, 4.3),
+        createData('Marshmallow', 318, 0, 81, 2.0, 67, 4.3),
+        createData('Nougat', 360, 19.0, 9, 37.0, 67, 4.3),
+        createData('Oreo', 437, 18.0, 63, 4.0, 67, 4.3),
+      ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+      page: 0,
+      rowsPerPage: 5,
     };
 
     this.fetchData = this.fetchData.bind(this);
@@ -157,7 +242,6 @@ class ClientManage extends Component {
   componentDidMount() {
 
       var that = this;
-  
       axios({
         method: "post",
         url: "http://localhost:8080/gpms/readClientGroupList",
@@ -188,7 +272,10 @@ class ClientManage extends Component {
 
   fetchData(state, instance) {
 
-    this.setState({ loading: true });
+    this.setState({
+      loading: true,
+      pageSize: state.pageSize
+    });
     const that = this;
     axios({
       method: "post",
@@ -199,7 +286,7 @@ class ClientManage extends Component {
       }],
       data: {
         start: "0",
-        length: "5",
+        length: state.pageSize,
         searchKey: that.state.keyword,
         clientType: that.state.clientStatus,
         groupId: that.state.clientGroup,
@@ -225,14 +312,71 @@ class ClientManage extends Component {
       .catch(function(error) {
         console.log(error);
       });
-
   }
 
+  // .................................................
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    const data =
+      order === 'desc'
+        ? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+        : this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
+
+    this.setState({ data, order, orderBy });
+  };
+
+  handleSelectAllClick = (event, checked) => {
+    if (checked) {
+      this.setState({ selected: this.state.data.map(n => n.id) });
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+
+  // .................................................
 
 
 
 
-
+  // Events...
   handleChangeSelect = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
@@ -242,10 +386,12 @@ class ClientManage extends Component {
   };
 
   render() {
-    //console.log("-ClientManage.render-------------------------");
     const { classes } = this.props;
     const { clientListData, clientGroup, clientStatus, keyword, pages, loading } = this.state;
-    //console.log(this.state.clientGroupOptionList);
+
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     return (
       <div className={classes.root}>
         <Card className={classes.content}>
@@ -306,14 +452,67 @@ class ClientManage extends Component {
               </Button>
             </form>
 
-            <ReactTable
-              data={clientListData}
-              columns={GrGridColumns}
-              loading={loading}
-              defaultPageSize={5}
-              onFetchData={this.fetchData}
-              
+    <div className={classNames(classes.tableWrapper, classes.tableContainer)}>
+          <Table className={classes.table}>
+            <ClientManageHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={this.handleSelectAllClick}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
             />
+            <TableBody>
+              {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+                const isSelected = this.isSelected(n.id);
+                return (
+                  <TableRow
+                    hover
+                    onClick={event => this.handleClick(event, n.id)}
+                    role="checkbox"
+                    aria-checked={isSelected}
+                    tabIndex={-1}
+                    key={n.id}
+                    selected={isSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox checked={isSelected} />
+                    </TableCell>
+                    <TableCell padding="none">{n.name}</TableCell>
+                    <TableCell numeric>{n.calories}</TableCell>
+                    <TableCell numeric>{n.fat}</TableCell>
+                    <TableCell numeric>{n.carbs}</TableCell>
+                    <TableCell numeric>{n.protein}</TableCell>
+                    <TableCell numeric>{n.carbs}</TableCell>
+                    <TableCell numeric>{n.protein}</TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+
+
+
           </CardContent>
         </Card>
       </div>
@@ -322,7 +521,6 @@ class ClientManage extends Component {
 }
 
 ClientManage.propTypes = {
-  children: PropTypes.node,
   classes: PropTypes.object.isRequired
 };
 export default withStyles(styles)(ClientManage);
