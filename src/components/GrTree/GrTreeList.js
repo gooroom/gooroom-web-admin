ce1c3a829e4aba3f27ab21d272f0158966a29706
sangyong han <syhan@gooroom.kr>
2018-04-26 17:10:36 +0900

    트리 완성 (전체선택, 부분선택감지및적용)

--- src/components/GrTree/GrTreeList.js
+++ src/components/GrTree/GrTreeList.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import classNames from "classnames";
+import { createMuiTheme } from 'material-ui/styles';
+import { css } from 'glamor';
 
 import { grRequestPromise } from "../../components/GrUtils/GrRequester";
 
@@ -12,66 +15,18 @@ import CloseIcon from "@material-ui/icons/ExpandLess";
 import FolderIcon from "@material-ui/icons/Folder";
 import FileIcon from "@material-ui/icons/InsertDriveFile";
 
-const listItems = [
-  {
-    key: "tree_0",
-    depth: 0,
-    disabled: false,
-    title: "treenode0",
-    children: ["0", "0", "0"]
-  },
-  {
-    key: "tree_1",
-    depth: 1,
-    parentIndex: 0,
-    disabled: false,
-    title: "treenode1"
-  },
-  {
-    key: "tree_2",
-    depth: 1,
-    parentIndex: 0,
-    disabled: false,
-    title: "treenode2",
-    children: ["3"]
-  },
-  {
-    key: "tree_4",
-    depth: 2,
-    parentIndex: 2,
-    disabled: false,
-    title: "treenode4"
-  },
-  {
-    key: "tree_3",
-    depth: 1,
-    parentIndex: 0,
-    disabled: false,
-    title: "treenode3",
-    children: ["5", "6"]
-  },
-  {
-    key: "tree_6",
-    depth: 2,
-    parentIndex: 4,
-    disabled: false,
-    title: "treenode6"
-  },
-  {
-    key: "tree_7",
-    depth: 2,
-    parentIndex: 4,
-    disabled: false,
-    title: "treenode7"
-  },
-  {
-    key: "tree_5",
-    depth: 1,
-    parentIndex: 0,
-    disabled: true,
-    title: "treenode5"
-  }
-];
+
+//
+//  ## Style ########## ########## ########## ########## ########## 
+//
+const parentNodeClass = css({
+  color: "#686868"
+}).toString();
+
+const childNodeClass = css({
+  color: "#a8a8a8"
+}).toString();
+
 
 class GrTreeList extends Component {
   constructor(props) {
@@ -87,6 +42,7 @@ class GrTreeList extends Component {
       rootKeyValue: props.rootKeyValue,
       keyName: props.keyName,
       title: props.title,
+      startingDepth: props.startingDepth ? props.startingDepth: 0,
 
       treeData: [],
 
@@ -134,17 +90,29 @@ class GrTreeList extends Component {
         parents[index].children = resData.map(d => {
           return d.key;
         });
+        
+        if(this.state.checked.includes(parents[index].key)) {
+          // add checked data, by default checked.
+          const newChecked = [...this.state.checked];
+          resData.map(d => {
+            newChecked.push(d.key);  
+            return d;
+          });
+          this.setState({
+            checked: newChecked  
+          });
+        }
         // data merge.
         // use SPLICE, CONCAT for insert in array
         parents.splice.apply(parents, [index + 1, 0].concat(resData));
-
         // reset parentIndex with node after (index + redData.length)
         parents = parents.map((obj, i) => {
-          if (i > index + resData.length && obj.depth > parents[index].depth) {
+          if (i > index + resData.length && obj.parentIndex > this.state.startingDepth) {
             obj.parentIndex = obj.parentIndex + resData.length;
           }
           return obj;
         });
+
         // reset expandedListItems values for adding nodes.
         const expandedListItems = this.state.expandedListItems;
         const newExpandedListItems = expandedListItems.map(obj => {
@@ -159,7 +127,9 @@ class GrTreeList extends Component {
             expandedListItems: newExpandedListItems,
             treeData: parents
         });
+
       } else {
+
         this.setState({
           treeData: resData
         });
@@ -168,16 +138,16 @@ class GrTreeList extends Component {
   }
 
   handleClickNode(listItem, index) {
+    // console.log("handleClickNode... " + listItem.key + ", " + index);
+    // console.log("handleClickNode..expandedListItems. " + this.state.expandedListItems);
     if (listItem.children) {
       // fetch children data
       // request to server if children array is empty.
-      if (listItem.children < 1) {
+      if (listItem.children.length < 1) {
         this.fetchTreeData(listItem.key, index);
       }
 
-      const indexOfListItemInArray = this.state.expandedListItems.indexOf(
-        index
-      );
+      const indexOfListItemInArray = this.state.expandedListItems.indexOf(index);
       if (indexOfListItemInArray === -1) {
         this.setState({
           expandedListItems: this.state.expandedListItems.concat([index])
@@ -190,8 +160,8 @@ class GrTreeList extends Component {
         });
       }
     } else {
-    }
 
+    }
     // select node
     this.setState({
       activeListItem: index
@@ -202,48 +172,57 @@ class GrTreeList extends Component {
 
   updateCheckStatus = (nodeKey, tempChecked, isChecked) => {
     const newChecked = [...tempChecked];
-    const currentIndex = tempChecked.indexOf(nodeKey);
+    const currentCheckedIndex = tempChecked.indexOf(nodeKey);
     if (isChecked) {
-      if (currentIndex === -1) {
+      if (currentCheckedIndex === -1) {
         newChecked.push(nodeKey);
       }
     } else {
-      if (currentIndex !== -1) {
-        newChecked.splice(currentIndex, 1);
+      if (currentCheckedIndex !== -1) {
+        newChecked.splice(currentCheckedIndex, 1);
       }
     }
     return newChecked;
   }
 
-  updateChangeNode = (nodeKey, isChecked, newChecked, newImperfect) => {
+  updateParentNode = (nodeKey, isChecked, newChecked, newImperfect) => {
 
     const { treeData } = this.state;
-
     const targetNode = treeData.filter(obj => obj.key === nodeKey)[0];
     let isCheckList = null;
     if(targetNode.parentIndex !== undefined) {
 
         isCheckList = treeData[targetNode.parentIndex].children.map(obj => {
-            return (newChecked.includes(obj) || (newImperfect.includes(obj)));
+            return (newChecked.includes(obj) && !(newImperfect.includes(obj)));
+        });
+        // test one more for newImperfect
+        const isImperfect = treeData[targetNode.parentIndex].children.map(obj => {
+          return (newImperfect.includes(obj));
         });
 
         const pIndex = treeData.filter(obj => obj.key === nodeKey)[0].parentIndex;
-        if(isCheckList.every(obj => {if(obj) return obj;})) {
-            // all checked.
-            newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
-            newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, false);
-        } else if(isCheckList.every(obj => {if(!obj) return !obj;})) {
-            // all unchecked.
-            newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, false);
-            newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, false);
-        } else {
+        if(isImperfect.includes(true)) {
             // parted checked
             newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
             newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, true);
+        } else {
+          if(isCheckList.every(obj => {if(obj) return obj;})) {
+              // all checked.
+              newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
+              newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, false);
+          } else if(isCheckList.every(obj => {if(!obj) return !obj;})) {
+              // all unchecked.
+              newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, false);
+              newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, false);
+          } else {
+              // parted checked
+              newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
+              newImperfect = this.updateCheckStatus(treeData[pIndex].key, newImperfect, true);
+          }
         }
 
         if(pIndex >= 0) {
-            const newStatus = this.updateChangeNode(treeData[pIndex].key, isChecked, newChecked, newImperfect);
+            const newStatus = this.updateParentNode(treeData[pIndex].key, isChecked, newChecked, newImperfect);
             newChecked = newStatus.newChecked;
             newImperfect = newStatus.newImperfect;
         }
@@ -256,45 +235,63 @@ class GrTreeList extends Component {
 
   }
 
-  handleChange = nodeKey => event => {
-    console.log(
-      "[handleChange] ... " + nodeKey + " -> " + event.target.checked
-    );
-    const { checked, imperfect } = this.state;
+  updateChildrenNode = (subNodes, isChecked, newChecked, newImperfect) => {
 
-    let newImperfect = imperfect;
-    let newChecked = this.updateCheckStatus(nodeKey, checked, event.target.checked);
-
-    const newStatus = this.updateChangeNode(nodeKey, event.target.checked, newChecked, newImperfect);
-/*
-    const { checked, imperfect, treeData } = this.state;
-    let newChecked = this.updateCheckStatus(nodeKey, checked, event.target.checked);
-    let newImperfect = [];
-
-    const targetNode = treeData.filter(obj => obj.key === nodeKey)[0];
-    let isCheckList = null;
-    if(targetNode.parentIndex !== undefined) {
-
-        isCheckList = treeData[targetNode.parentIndex].children.map(obj => {
-            return (newChecked.includes(obj) && !(imperfect.includes(obj)));
-        });
-
-        const pIndex = treeData.filter(obj => obj.key === nodeKey)[0].parentIndex;
-        if(isCheckList.every(obj => {if(obj) return obj;})) {
-            // all checked.
-            newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
-            newImperfect = this.updateCheckStatus(treeData[pIndex].key, imperfect, false);
-        } else if(isCheckList.every(obj => {if(!obj) return !obj;})) {
-            // all unchecked.
-            newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, false);
-            newImperfect = this.updateCheckStatus(treeData[pIndex].key, imperfect, false);
+    const { treeData } = this.state;
+    if(subNodes && subNodes.length > 0) {
+      for(var i = 0; i < subNodes.length; i++) {
+        if(isChecked) {
+          newChecked = this.updateCheckStatus(subNodes[i], newChecked, true);
+          newImperfect = this.updateCheckStatus(subNodes[i], newImperfect, false);
         } else {
-            // parted checked
-            newChecked = this.updateCheckStatus(treeData[pIndex].key, newChecked, true);
-            newImperfect = this.updateCheckStatus(treeData[pIndex].key, imperfect, true);
+          newChecked = this.updateCheckStatus(subNodes[i], newChecked, false);
+          newImperfect = this.updateCheckStatus(subNodes[i], newImperfect, false);
         }
+
+        const children = treeData.filter(obj => obj.key === subNodes[i])[0].children;
+        if(children) {
+          const newStatus = this.updateChildrenNode(children, isChecked, newChecked, newImperfect);
+          newChecked = newStatus.newChecked;
+          newImperfect = newStatus.newImperfect;
+        }
+      }
     }
-*/
+
+    return {
+      newChecked: newChecked,
+      newImperfect: newImperfect
+    };
+  }
+
+  handleChange = nodeKey => event => {
+    // console.log("[handleChange] ... " + nodeKey + " -> " + event.target.checked);
+    const { checked, imperfect, treeData } = this.state;
+
+    let newChecked = checked;
+    let newImperfect = imperfect;
+
+    const children = treeData.filter(obj => obj.key === nodeKey)[0].children;
+    if(children) {
+      if(event.target.checked) {
+        // check self and children
+        newChecked = this.updateCheckStatus(nodeKey, newChecked, true);
+      } else {
+        // uncheck self and children
+        newChecked = this.updateCheckStatus(nodeKey, newChecked, false);
+      }
+      // remove from imperfect
+      newImperfect = this.updateCheckStatus(nodeKey, newImperfect, false);
+      // check children from this
+      const newChildrenStatus = this.updateChildrenNode(children, event.target.checked, newChecked, newImperfect);
+      newChecked = newChildrenStatus.newChecked;
+      newImperfect = newChildrenStatus.newImperfect;
+    } else {
+      newChecked = this.updateCheckStatus(nodeKey, checked, event.target.checked);
+    }
+
+    // check parent from this
+    const newStatus = this.updateParentNode(nodeKey, event.target.checked, newChecked, newImperfect);
+
     this.setState({
       checked: newStatus.newChecked,
       imperfect: newStatus.newImperfect
@@ -305,9 +302,7 @@ class GrTreeList extends Component {
   render() {
     const contentKey = "title";
 
-    const startingDepth = this.props.startingDepth
-      ? this.props.startingDepth
-      : 0;
+    const startingDepth = this.state.startingDepth;
     const listHeight = this.props.listHeight ? this.props.listHeight : "36px";
     const activeListItem = this.props.activeListItem
       ? this.props.activeListItem
@@ -332,8 +327,8 @@ class GrTreeList extends Component {
           }
         };
 
-        listItem._shouldRender =
-          listItem.depth >= startingDepth && parentsAreExpanded(listItem);
+        listItem._shouldRender = //(listItem._shouldRender) ||
+          (listItem.depth >= startingDepth && parentsAreExpanded(listItem));
         listItem._primaryText = listItem[contentKey];
 
         return listItem;
@@ -386,9 +381,9 @@ class GrTreeList extends Component {
     function getLeftIcon(listItem, useFolderIcons) {
       if (useFolderIcons) {
         if (listItem.children) {
-          return <FolderIcon />;
+          return <FolderIcon className={parentNodeClass} />;
         } else {
-          return <FileIcon />;
+          return <FileIcon className={childNodeClass} />;
         }
       } else {
         return listItem.icon;
