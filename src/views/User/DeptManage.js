bf9323b8639053893b866d945061c42d9eb860fa
sangyong han <syhan@gooroom.kr>
2018-04-26 18:05:08 +0900

    조직 관리 화면 일부 완성 - 조직트리 완성, 조직상세 완성, 사용자리스트 조회는 사용자 메뉴 완성후 컴포넌트 연결하기로함.

--- src/views/User/DeptManage.js
+++ src/views/User/DeptManage.js
@@ -11,8 +11,9 @@ import { grRequestPromise } from "../../components/GrUtils/GrRequester";
 import GrPageHeader from "../../components/GrHeader/GrPageHeader";
 
 import GrTreeList from "../../components/GrTree/GrTreeList";
-
 import GrPane from "../../containers/Container/GrPane";
+
+import Typography from 'material-ui/Typography';
 
 import Grid from "material-ui/Grid";
 import Paper from "material-ui/Paper";
@@ -31,38 +32,104 @@ const pageContentClass = css({
   paddingTop: "14px !important"
 }).toString();
 
+const treeCardClass = css({
+  minHeight: "600px !important"
+}).toString();
+
+const deptInfoCardClass = css({
+  marginBottom: "0px",
+}).toString();
+
+const deptUserCardClass = css({
+  marginTop: "20px",
+}).toString();
+
+
+const bulletClass = css({
+  display: "inline-block",
+  margin: "0 2px",
+  transform: "scale(0.8)",
+}).toString();
+
+const titleClass = css({
+  marginBottom: "16px",
+  fontSize: "14px",
+}).toString();
+
+const posClass = css({
+  marginBottom: "12px",
+}).toString();
+
 class DeptManage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      deptName: "",
+      deptId: ""
+    }
 
     this.handleSelectDept = this.handleSelectDept.bind(this);
   }
 
   handleSelectDept(node) {
-    console.log("..... DeptManage handleSelectDept... " + node.title);
+    this.setState({
+      deptName: node.title,
+      deptId: node.key
+    });
+    console.log("..... DeptManage handleSelectDept... " + node.title + ", " + node.key);
   }
 
   render() {
+    const bull = <span className={bulletClass}>•</span>;
+
+    const { deptName, deptId } = this.state;
+
     return (
       <React.Fragment>
         <GrPageHeader path={this.props.location.pathname} />
         <GrPane>
           <Grid container spacing={24}>
-            <Grid item xs={12} sm={4}>
-            <GrTreeList
-            useFolderIcons={true}
-            listHeight="24px"
-            url="http://localhost:8080/gpms/readChildrenDeptList"
-            paramKeyName="deptCd"
-            rootKeyValue="000000000"
-            keyName="key"
-            title="title"
-            startingDepth="2"
-            onSelectNode={this.handleSelectDept}
-          />
+            <Grid item xs={12} sm={5}>
+              <Card className={treeCardClass}>
+                <GrTreeList
+                  useFolderIcons={true}
+                  listHeight="24px"
+                  url="http://localhost:8080/gpms/readChildrenDeptList"
+                  paramKeyName="deptCd"
+                  rootKeyValue="000000000"
+                  keyName="key"
+                  title="title"
+                  startingDepth="2"
+                  onSelectNode={this.handleSelectDept}
+                />
+              </Card>
             </Grid>
-            <Grid item xs={12} sm={8}>
-              <Paper>xs=12 sm=6</Paper>
+            <Grid item xs={12} sm={7}>
+              <Card className={deptInfoCardClass}>
+                <CardContent>
+                  <Typography className={titleClass} color="textSecondary">
+                  조직 정보
+                  </Typography>
+                  <Typography variant="headline" component="h2">
+                  {deptName}
+                  </Typography>
+                  <Typography className={posClass} color="textSecondary">
+                  {bull} ID - {deptId}
+                  </Typography>
+                  <Typography component="p">
+                    {bull} {'"이 조직에 대한 설명 또는 상세 정보"'}
+                  </Typography>
+                
+                </CardContent>
+              </Card>
+              <Card className={deptUserCardClass}>
+                <CardContent>
+                  <Typography className={titleClass} color="textSecondary">
+                  사용자 정보
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </GrPane>
