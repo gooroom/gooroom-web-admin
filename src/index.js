aa8f35afdb6a25e37ca099763e677870a8f2c4d9
sangyong han <syhan@cloudrim.co.kr>
2018-05-30 19:24:15 +0900

    redux 적용중

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 // Import Main styles for this application
 import './scss/main.scss'
@@ -8,7 +12,11 @@ import './scss/main.scss'
 // Containers
 import GrFull from './containers/GrFull/'
 
+// Create Store
+const store = createStore(reducers);
+
 ReactDOM.render(
+  <Provider store={store}>
   <HashRouter>
     <Switch>
 
@@ -16,6 +24,7 @@ ReactDOM.render(
 
       <Route path="/" name="Home" component={GrFull} />
     </Switch>
-  </HashRouter>,
-  document.getElementById("root")
+  </HashRouter>
+  </Provider>,
+  document.getElementById('root')
 );
