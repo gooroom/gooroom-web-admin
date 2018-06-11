9299f952fdb3676678d00a5a6fcb1ff67f96fec6
sangyong han <syhan@gooroom.kr>
2018-06-11 15:47:07 +0900

    프로파일 CRUD 완료, 단말 선택 추가해서 붙여야 함.

--- src/modules/clientProfileSetModule.js
+++ src/modules/clientProfileSetModule.js
@@ -23,13 +23,22 @@ const GET_PROFILESET_LIST_PENDING = 'clientProfileSet/GET_LIST_PENDING';
 const GET_PROFILESET_LIST_SUCCESS = 'clientProfileSet/GET_LIST_SUCCESS';
 const GET_PROFILESET_LIST_FAILURE = 'clientProfileSet/GET_LIST_FAILURE';
 
-const SET_PROFILESET_DATA_PENDING = 'clientProfileSet/SET_DATA_PENDING';
-const SET_PROFILESET_DATA_SUCCESS = 'clientProfileSet/SET_DATA_SUCCESS';
-const SET_PROFILESET_DATA_FAILURE = 'clientProfileSet/SET_DATA_FAILURE';
+const CREATE_PROFILESET_DATA_PENDING = 'clientProfileSet/CREATE_DATA_PENDING';
+const CREATE_PROFILESET_DATA_SUCCESS = 'clientProfileSet/CREATE_DATA_SUCCESS';
+const CREATE_PROFILESET_DATA_FAILURE = 'clientProfileSet/CREATE_DATA_FAILURE';
+
+const EDIT_PROFILESET_DATA_PENDING = 'clientProfileSet/EDIT_DATA_PENDING';
+const EDIT_PROFILESET_DATA_SUCCESS = 'clientProfileSet/EDIT_DATA_SUCCESS';
+const EDIT_PROFILESET_DATA_FAILURE = 'clientProfileSet/EDIT_DATA_FAILURE';
+
+const DELETE_PROFILESET_DATA_PENDING = 'clientProfileSet/DELETE_DATA_PENDING';
+const DELETE_PROFILESET_DATA_SUCCESS = 'clientProfileSet/DELETE_DATA_SUCCESS';
+const DELETE_PROFILESET_DATA_FAILURE = 'clientProfileSet/DELETE_DATA_FAILURE';
 
 const SHOW_PROFILESET_DATA = 'clientProfileSetPopup/SHOW_PROFILESET_DATA';
 const CLOSE_PROFILESET_DATA = 'clientProfileSetPopup/CLOSE_PROFILESET_DATA';
 const CHG_PROFILESET_PARAM = 'clientProfileSetPopup/CHG_PROFILESET_PARAM';
+const SET_PROFILESET_SELECTED = 'clientProfileSetPopup/SET_PROFILESET_SELECTED';
 // ...
 
 
@@ -62,20 +71,64 @@ export const readClientProfileSetList = (param) => dispatch => {
     });
 };
 
-export const setClientProfileSetData = (param) => dispatch => {
-    dispatch({type: SET_PROFILESET_DATA_PENDING});
+// create (add)
+export const createClientProfileSetData = (param) => dispatch => {
+    dispatch({type: CREATE_PROFILESET_DATA_PENDING});
     return requestPostAPI('createProfileSet', param).then(
         (response) => {
             dispatch({
-                type: SET_PROFILESET_DATA_SUCCESS,
+                type: CREATE_PROFILESET_DATA_SUCCESS,
                 payload: response
             });
         }
     ).catch(error => {
         dispatch({
-            type: SET_PROFILESET_DATA_FAILURE,
+            type: CREATE_PROFILESET_DATA_FAILURE,
             payload: error
         });
+    });
+};
+
+// edit
+export const editClientProfileSetData = (param) => dispatch => {
+    dispatch({type: EDIT_PROFILESET_DATA_PENDING});
+    return requestPostAPI('editProfileSetData', param).then(
+        (response) => {
+            dispatch({
+                type: EDIT_PROFILESET_DATA_SUCCESS,
+                payload: response
+            });
+        }
+    ).catch(error => {
+        dispatch({
+            type: EDIT_PROFILESET_DATA_FAILURE,
+            payload: error
+        });
+    });
+};
+
+// delete
+export const deleteClientProfileSetData = (param) => dispatch => {
+    dispatch({type: DELETE_PROFILESET_DATA_PENDING});
+    return requestPostAPI('deleteProfileSetData', param).then(
+        (response) => {
+            dispatch({
+                type: DELETE_PROFILESET_DATA_SUCCESS,
+                payload: response
+            });
+        }
+    ).catch(error => {
+        dispatch({
+            type: DELETE_PROFILESET_DATA_FAILURE,
+            payload: error
+        });
+    });
+};
+
+export const setSelectedItem = (param) => dispatch => {
+    return dispatch({
+        type: SET_PROFILESET_SELECTED,
+        payload: param
     });
 };
 
@@ -112,6 +165,7 @@ const initialState = {
     orderColumn: '',
 
     selectedItem: {
+        profileNo: '',
         profileName: '',
         profileComment: ''
     },
@@ -157,21 +211,21 @@ export default handleActions({
             error: true
         };
     },
-    [SET_PROFILESET_DATA_PENDING]: (state, action) => {
+    [CREATE_PROFILESET_DATA_PENDING]: (state, action) => {
         return {
             ...state,
             pending: true,
             error: false
         };
     },
-    [SET_PROFILESET_DATA_SUCCESS]: (state, action) => {
+    [CREATE_PROFILESET_DATA_SUCCESS]: (state, action) => {
         return {
             ...state,
             pending: false,
             error: false,
         };
     },
-    [SET_PROFILESET_DATA_FAILURE]: (state, action) => {
+    [CREATE_PROFILESET_DATA_FAILURE]: (state, action) => {
         return {
             ...state,
             pending: false,
@@ -179,13 +233,14 @@ export default handleActions({
         };
     },
     [SHOW_PROFILESET_DATA]: (state, action) => {
-        console.log('SHOW_PROFILESET_DATA - state: ', state);
-        console.log('SHOW_PROFILESET_DATA - action: ', action);
         return {
             ...state,
             selectedItem: action.payload.selectedItem,
             dialogOpen: action.payload.dialogOpen,
-            dialogType: action.payload.dialogType
+            dialogType: action.payload.dialogType,
+            profileName: ((action.payload.selectedItem) ? action.payload.selectedItem.profileName : ''),
+            profileComment: ((action.payload.selectedItem) ? action.payload.selectedItem.profileComment : ''),
+            clientId: ((action.payload.selectedItem) ? action.payload.selectedItem.clientId : '')
         };
     },
     [CLOSE_PROFILESET_DATA]: (state, action) => {
@@ -199,7 +254,14 @@ export default handleActions({
             ...state,
             [action.payload.name]: action.payload.value
         }
+    },
+    [SET_PROFILESET_SELECTED]: (state, action) => {
+        return {
+            ...state,
+            selectedItem: action.payload.selectedItem
+        }
     }
+
 }, initialState);
 
 
