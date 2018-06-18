c93d37c06586ff96e54eeeb1ac7425e3138407ee
sangyong han <syhan@cloudrim.co.kr>
2018-06-18 19:22:15 +0900

    edit job manager to redux pattern. - ..ing.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import ClientRegKeyModule from './ClientRegKeyModule';
 import ClientProfileSetModule from './ClientProfileSetModule';
+import JobManageModule from './JobManageModule';
 import GrConfirmModule from './GrConfirmModule';
 
 export default combineReducers({
-    ClientRegKeyModule, ClientProfileSetModule, GrConfirmModule
+    ClientRegKeyModule, ClientProfileSetModule, JobManageModule, GrConfirmModule
 });
