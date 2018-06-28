e3b42a200dfaf141108e5f513571b919d46a7749
sangyong han <syhan@cloudrim.co.kr>
2018-06-28 18:22:37 +0900

    단말관리를 REDUX 로 변환중.

---
+++ src/views/Options/ClientGroupSelect.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as CommonOptionActions from '../../modules/CommonOptionModule';
+
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
+//
+//  ## Content ########## ########## ########## ########## ########## 
+//
+class ClientGroupSelect extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+
+    const { CommonOptionActions, CommonOptionProps } = this.props;
+    CommonOptionActions.readClientGroupListAll(CommonOptionProps.listParam);
+  }
+
+  // Events...
+  handleChangeSelect = event => {
+    const { CommonOptionActions } = this.props;
+    CommonOptionActions.changeSelectValue({
+      name: 'selectedClientGroup',
+      value: {
+        grpId: event.target.value,
+        grpNm: event.target.name
+      }
+    });
+    this.props.onChangeSelect(event, event.target.value);
+  };
+
+  render() {
+
+    const { CommonOptionProps } = this.props;
+
+    return (
+
+      <Select
+        value={CommonOptionProps.selectedClientGroup.grpId}
+        onChange={this.handleChangeSelect}
+        inputProps={{name: 'clientGroup'}}
+      >
+        {CommonOptionProps.listDataForClientGroupSelect.map(x => (
+          <MenuItem value={x.grpId} key={x.grpId}>
+            {x.grpNm}
+          </MenuItem>
+        ))}
+      </Select>
+
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  CommonOptionProps: state.CommonOptionModule
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  CommonOptionActions: bindActionCreators(CommonOptionActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClientGroupSelect);
+
+
