236b6ecd85807163b0499ff7e651eb0af5813f36
sangyong han <syhan@gooroom.kr>
2018-07-17 10:03:44 +0900

    컴포넌트 임포트를 새로운 형식으로 변경

--- src/views/User/UserManageDialog.js
+++ src/views/User/UserManageDialog.js
@@ -6,12 +6,19 @@ import { withTheme } from "@material-ui/core/styles";
 
 import { css } from "glamor";
 
-import Dialog, {DialogTitle, DialogActions } from "@material-ui/core/Dialog";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 import Typography from '@material-ui/core/Typography';
-import Card, { CardHeader, CardMedia, CardContent, CardActions } from "@material-ui/core/Card";
+
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 
 //
 //  ## Style ########## ########## ########## ########## ##########
