ddac1c8293bbc9d3684b0dc4fda35f7a3fdada44
sangyong han <syhan@gooroom.kr>
2018-08-29 20:11:08 +0900

    immutable.js 작업중.

--- src/components/GrUtils/GrTableListUtils.js
+++ src/components/GrUtils/GrTableListUtils.js
@@ -1,34 +1,38 @@
+import { List } from "@material-ui/core";
 
 export const getTableListObject = (propObj, compId) => {
 
-    let viewItem = null;
-    if(propObj.viewItems) {
-      viewItem = propObj.viewItems.find((element) => {
-        return element._COMPID_ == compId;
-      });
-    }
+    console.log('propObj >> ', propObj);
+
+    // if(propObj.get('viewItems')) {
+    //   viewItem = propObj.get('viewItems').find((element) => {
+    //     return element._COMPID_ == compId;
+    //   });
+
+    //   return viewItem.toJS();
+    // }
 
     return {
-        listData: (viewItem) ? viewItem.listData : [],
-        listParam: (viewItem) ? viewItem.listParam : propObj.defaultListParam,
-        orderDir: (viewItem && viewItem.listParam) ? viewItem.listParam.orderDir : propObj.defaultListParam.orderDir,
-        orderColumn: (viewItem && viewItem.listParam) ? viewItem.listParam.orderColumn : propObj.defaultListParam.orderColumn,
+        // listData: List([]),
+        // listParam: propObj.get('defaultListParam'),
+        // orderDir: (viewItem && viewItem.listParam) ? viewItem.listParam.orderDir : propObj.defaultListParam.orderDir,
+        // orderColumn: (viewItem && viewItem.listParam) ? viewItem.listParam.orderColumn : propObj.defaultListParam.orderColumn,
     }
 }
 
 export const getTableSelectedObject = (propObj, compId) => {
 
-  if(propObj && propObj.viewItems) {
-    const viewItem = propObj.viewItems.find(function(element) {
-      return element._COMPID_ == compId;
-    });
+  // if(propObj && propObj.viewItems) {
+  //   const viewItem = propObj.viewItems.find(function(element) {
+  //     return element._COMPID_ == compId;
+  //   });
   
-    if(viewItem) {
-      return viewItem.selectedItem;
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
+  //   if(viewItem) {
+  //     return viewItem.selectedItem;
+  //   } else {
+  //     return null;
+  //   }
+  // } else {
+     return null;
+  // }
 }
