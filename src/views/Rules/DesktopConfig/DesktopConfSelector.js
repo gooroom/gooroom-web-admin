b9f8f1d292bffba4f3bc6a24b95121598ec8cca2
sangyong han <syhan@gooroom.kr>
2018-10-23 17:09:11 +0900

    데스크톱정보 관리 개발중

---
+++ src/views/Rules/DesktopConfig/DesktopConfSelector.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { Map, List, fromJS } from 'immutable';
+
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as DesktopConfActions from 'modules/DesktopConfModule';
+import DesktopConfSpec from 'views/Rules/UserConfig/DesktopConfSpec';
+import DesktopConfDialog from 'views/Rules/UserConfig/DesktopConfDialog';
+
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import { withStyles } from '@material-ui/core/styles';
+import { GRCommonStyle } from 'templates/styles/GRStyles';
+
+//
+//  ## Content ########## ########## ########## ########## ########## 
+//
+class DesktopConfSelector extends Component {
+
+  componentDidMount() {
+    const { DesktopConfProps, DesktopConfActions, compId, initId, targetType } = this.props;
+    //
+    DesktopConfActions.readDesktopConfList(DesktopConfProps, compId, targetType);
+    //
+    const targetNames = (targetType && targetType != '') ? ['viewItems', compId, targetType] : ['viewItems', compId];
+    if(!DesktopConfProps.getIn(List(targetNames).push('selectedOptionItemId'))) {
+      DesktopConfActions.changeCompVariable({
+        compId: compId,
+        name: 'selectedOptionItemId',
+        value: initId,
+        targetType: targetType
+      });
+    }
+  }
+  
+  handleChange = (event, value) => {
+    const { DesktopConfActions, compId, targetType } = this.props;
+    DesktopConfActions.changeCompVariable({
+      compId: compId,
+      name: 'selectedOptionItemId',
+      value: event.target.value,
+      targetType: targetType
+    });
+  };
+
+  // ===================================================================
+  handleEditClickForDesktopConf = (viewItem, compType) => {
+    this.props.DesktopConfActions.showDialog({
+      selectedViewItem: viewItem,
+      dialogType: DesktopConfDialog.TYPE_EDIT
+    });
+  };
+  // ===================================================================
+  
+  // .................................................
+  render() {
+    const { classes } = this.props;
+    const { DesktopConfProps, compId, targetType } = this.props;
+
+    const selectedObj = (targetType && targetType != '') ? DesktopConfProps.getIn(['viewItems', compId, targetType]) : DesktopConfProps.getIn(['viewItems', compId]);
+
+    const listAllData = (selectedObj) ? selectedObj.get('listAllData') : null;
+    let selectedOptionItemId = (selectedObj) ? selectedObj.get('selectedOptionItemId') : null;
+    if(!selectedOptionItemId && listAllData && listAllData.size > 0) {
+      selectedOptionItemId = '-';
+    }
+
+    let selectedDesktopConfItem = null;
+    if(listAllData && listAllData.size > 0) {
+      const selectedData = listAllData.find((element) => {
+        return element.get('objId') == selectedOptionItemId;
+      });
+      if(selectedData) {
+        selectedDesktopConfItem = Map({'selectedViewItem': selectedData});
+      }      
+    };
+
+    return (
+      <Card className={classes.card}>
+        <CardContent style={{padding: 0}}>
+        {listAllData && 
+        <FormControl className={classes.formControl} style={{width: '100%', marginBottom: 24, marginTop: 8, border: 'dotted 1px lightGray'}}>
+          <InputLabel htmlFor="cfg-helper"></InputLabel>
+          <Select value={selectedOptionItemId}
+            onChange={this.handleChange}
+          >
+          <MenuItem key={'-'} value={'-'}>없음</MenuItem>
+          {listAllData.map(item => (
+            <MenuItem key={item.get('objId')} value={item.get('objId')}>{item.get('objNm')}</MenuItem>
+          ))}
+          </Select>
+          <FormHelperText>정책 정보를 선택하면 상세 내용이 표시됩니다.</FormHelperText>
+        </FormControl>
+        }
+        {selectedOptionItemId && selectedOptionItemId != '' &&
+          <DesktopConfSpec 
+            specType="inform" targetType={targetType}
+            selectedItem={selectedDesktopConfItem}
+            handleEditClick={this.handleEditClickForDesktopConf}
+          />
+        }
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  DesktopConfProps: state.DesktopConfModule
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  DesktopConfActions: bindActionCreators(DesktopConfActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(GRCommonStyle)(DesktopConfSelector));
+
+
