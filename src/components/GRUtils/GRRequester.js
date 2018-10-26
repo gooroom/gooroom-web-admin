1fd96c243ce0e3a2dd27a0a741b3b349ef7e1cae
sangyong han <syhan@gooroom.kr>
2018-10-26 15:08:36 +0900

    temp

--- src/components/GRUtils/GRRequester.js
+++ src/components/GRUtils/GRRequester.js
@@ -4,6 +4,8 @@ import qs from "qs";
 //export const GPMS_URL = 'http://localhost:8080/gpms/';
 export const GPMS_URL = 'http://ain.gooroom.kr:8080/gpms/';
 //export const GPMS_URL = 'https://gpms.gooroom.kr/gpms/';
+//export const GPMS_URL = '/gpms/';
+
 
 function collection() {
     let data = {};
