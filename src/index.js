8616a6b2fb3651d6870288f179deb61eb775b156
sangyong han <syhan@gooroom.kr>
2018-11-26 22:37:31 +0900

    라우터 관련 최종 정리 - 브라우저 라우터는 사용 못함 (세션문제) - 로그인 페이지는 스프링에서 수정해야함.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-import Root from './Root';
 import App from './App';
-
 
 import store from './store';
 import { Provider } from 'react-redux';
@@ -12,19 +10,10 @@ import { Provider } from 'react-redux';
 // Import Main styles for this application
 // import './scss/main.scss'
 
-// Containers
-import GRFull from 'containers/GRFull/'
-import Login from 'ui/login/Login';
-
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-    <Switch>
-
-      {/* list up another pages like 404, 500 error pages. */}
-      
-      <Route path="/" name="Home" component={GRFull} />
-    </Switch>
+    <HashRouter basename={'gpms'}>
+      <App />
     </HashRouter>
   </Provider>,
   document.getElementById('root')
