6f8bac6f0566d564cb54b0f86cc2c2208068368d
sangyong han <syhan@cloudrim.co.kr>
2018-12-18 14:40:48 +0900

    Ajax 호출시 944 코드가 리턴되면 세션 아웃으로 로그인 페이지로 이동-윈도우 리로드

--- src/components/GRUtils/GRRequester.js
+++ src/components/GRUtils/GRRequester.js
@@ -1,8 +1,8 @@
 import axios, { post }  from "axios";
 import qs from "qs";
 
-export const GPMS_URL = 'http://ain.gooroom.kr:8080/gpms/';
-//export const GPMS_URL = '/gpms/';
+//export const GPMS_URL = 'http://ain.gooroom.kr:8080/gpms/';
+export const GPMS_URL = '/gpms/';
 
 function collection() {
     let data = {};
@@ -65,10 +65,10 @@ export function grRequestPromise(url, param) {
       } else {
           reject(response);
       }
-      })
-      .catch(function(error) {
 
-      });
+    }).catch(function(error) {
+
+    });
   });
 };
 
@@ -76,17 +76,17 @@ export function grRequestPromise(url, param) {
 export function requestPostAPI(url, param, headers) {
 
   return axios({
-      method: "post",
-      url: GPMS_URL + url,
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      transformRequest: [
-        function(data, headers) {
-          return qs.stringify(data, {arrayFormat:'brackets'});
-        }
-      ],
-      data: param,
-      withCredentials: false
-    });
+    method: "post",
+    url: GPMS_URL + url,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    transformRequest: [
+      function(data, headers) {
+        return qs.stringify(data, {arrayFormat:'brackets'});
+      }
+    ],
+    data: param,
+    withCredentials: false
+  });
 };
 
 // multipartform
