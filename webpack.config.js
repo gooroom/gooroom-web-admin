d69e99400d892812f1a86f33c3d175050eda4c68
sangyong han <syhan@gooroom.kr>
2019-02-11 16:31:12 +0900

    modify webpack config and package.json : source refactoring, reuse proxy to server

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,19 @@ module.exports = (env = {}) => {
       hot: true,
       open: true,
       inline: true,
-      port: 9191
+      port: 9191,
+	    proxy: [{
+        context: [
+            '/gpms'
+        ],
+        target: `http://127.0.0.1:8080`,
+        secure: false,
+        changeOrigin: false,
+        headers: { host: 'localhost:9191' }
+      }],
+      watchOptions: {
+          ignored: /node_modules/
+      }
     },
   
     module: {
