4b50a1e3d4369df15f5a731a4bb2ae121612d03a
sangyong han <syhan@gooroom.kr>
2019-05-14 19:34:17 +0900

    단말그룹 등록창에 오류 수정 - 코드 위치 오류

--- src/views/ClientGroup/ClientGroupDialog.js
+++ src/views/ClientGroup/ClientGroupDialog.js
@@ -150,12 +150,12 @@ class ClientGroupDialog extends Component {
         const { ClientGroupProps, compId } = this.props;
         const { t, i18n } = this.props;
 
+        const dialogType = ClientGroupProps.get('dialogType');
+        const editingItem = (ClientGroupProps.get('editingItem')) ? ClientGroupProps.get('editingItem') : null;
+
         if(!(ClientGroupProps.get('dialogOpen') && editingItem)) {
             return (<React.Fragment></React.Fragment>);
         }
-
-        const dialogType = ClientGroupProps.get('dialogType');
-        const editingItem = (ClientGroupProps.get('editingItem')) ? ClientGroupProps.get('editingItem') : null;
 
         let title = "";
         if(dialogType === ClientGroupDialog.TYPE_ADD) {
