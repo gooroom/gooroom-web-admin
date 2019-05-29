e3051cfefa247a728bb745a21a85ceff170c19f7
sangyong han <syhan@gooroom.kr>
2019-05-29 12:28:07 +0900

    관리자 타입에 따른 수정 기능제어 : 브라우저정책,단말보안정책,소프트웨어정책,매체제어정책

--- src/views/Rules/UserConfig/SoftwareFilterManage.js
+++ src/views/Rules/UserConfig/SoftwareFilterManage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Map, List } from 'immutable';
-
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import * as Constants from "components/GRComponents/GRConstants";
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -85,7 +83,7 @@ class SoftwareFilterManage extends Component {
     });
   }
 
-  handleSelectRow = (event, id) => {
+  handleSelectRow = (event, id, isEditable) => {
     const { SoftwareFilterActions, SoftwareFilterProps } = this.props;
     const compId = this.props.match.params.grMenuId;
 
@@ -102,13 +100,19 @@ class SoftwareFilterManage extends Component {
     // 2. view detail content
     SoftwareFilterActions.showInform({
       compId: compId,
-      viewItem: viewItem
+      viewItem: viewItem,
+      isEditable: isEditable
     });
   };
 
   handleCreateButton = () => {
+    let adminType = 'A';
+    if(window.gpmsain === Constants.SUPER_RULECODE) {
+      adminType = 'S';
+    }
     this.props.SoftwareFilterActions.showDialog({
       viewItem: Map({
+        adminType: adminType,
         'SWITEM': Map({})
       }),
       dialogType: SoftwareFilterDialog.TYPE_ADD
@@ -230,33 +234,55 @@ class SoftwareFilterManage extends Component {
               />
               <TableBody>
                 {listObj.get('listData').map(n => {
+
+                  let isEditable = true;
+                  let isDeletable = true;
+                  if(n.get('objId').endsWith('DEFAULT') || n.get('objId').endsWith('STD')) {
+                    isEditable = false;
+                    isDeletable = false;
+                    if(window.gpmsain === Constants.SUPER_RULECODE) {
+                      isEditable = true;
+                      if(n.get('objId').endsWith('DEFAULT')) {
+                        isDeletable = false;
+                      } else {
+                        isDeletable = true;
+                      }
+                    }
+                  } else {
+                    if(window.gpmsain === Constants.SUPER_RULECODE) {
+                      isEditable = false;
+                      isDeletable = false;
+                    }
+                  }
+                  
                   return (
                     <TableRow 
                       hover
-                      onClick={event => this.handleSelectRow(event, n.get('objId'))}
+                      onClick={event => this.handleSelectRow(event, n.get('objId'), isEditable)}
                       tabIndex={-1}
                       key={n.get('objId')}
+                      className={(n.get('objId').endsWith('DEFAULT')) ? classes.grDefaultRuleRow : ((n.get('objId').endsWith('STD')) ? classes.grStandardRuleRow : "")}
                     >
-                      <TableCell className={classes.grSmallAndClickAndCenterCell}>{n.get('objId').endsWith('DEFAULT') ? t("selBasic") : t("selOrdinary")}</TableCell>
+                      <TableCell className={classes.grSmallAndClickAndCenterCell}>{n.get('objId').endsWith('DEFAULT') ? t("selBasic") : (n.get('objId').endsWith('STD') ? t("selStandard") : t("selOrdinary"))}</TableCell>
                       <TableCell className={classes.grSmallAndClickCell}>{n.get('objNm')}</TableCell>
                       <TableCell className={classes.grSmallAndClickAndCenterCell}>{n.get('objId')}</TableCell>
                       <TableCell className={classes.grSmallAndClickAndCenterCell}>{n.get('modUserId')}</TableCell>
                       <TableCell className={classes.grSmallAndClickAndCenterCell}>{formatDateToSimple(n.get('modDate'), 'YYYY-MM-DD')}</TableCell>
                       <TableCell className={classes.grSmallAndClickAndCenterCell}>
-
+                      {isEditable &&
                         <Button color="secondary" size="small" 
                           className={classes.buttonInTableRow}
                           onClick={event => this.handleEditListClick(event, n.get('objId'))}>
                           <SettingsApplicationsIcon />
                         </Button>
-
-                        { !n.get('objId').endsWith('DEFAULT') &&
+                      }
+                      {isDeletable &&
                         <Button color="secondary" size="small" 
                           className={classes.buttonInTableRow}
                           onClick={event => this.handleDeleteClick(event, n.get('objId'))}>
                           <DeleteIcon />
                         </Button>
-                        }
+                      }
 
                       </TableCell>
                     </TableRow>
@@ -293,6 +319,7 @@ class SoftwareFilterManage extends Component {
         {/* dialog(popup) component area */}
         <SoftwareFilterSpec compId={compId} specType="inform" 
           selectedItem={(listObj) ? listObj.get('viewItem') : null}
+          isEditable={(listObj) ? listObj.get('isEditable') : null}
           hasAction={true}
           onClickCopy={this.handleClickCopy}
           onClickEdit={this.handleClickEdit}
