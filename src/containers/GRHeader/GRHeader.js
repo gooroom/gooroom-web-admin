5682dac1bc8ca0526fad87fe6eab37445d6f100f
sangyong han <syhan@gooroom.kr>
2019-07-31 18:28:00 +0900

    버전 수정 : 0731

--- src/containers/GRHeader/GRHeader.js
+++ src/containers/GRHeader/GRHeader.js
@@ -63,7 +63,7 @@ class GRHeader extends Component {
       <AppBar className={classes.headerRoot}>
         <Toolbar className={classes.headerToolbar}>
             <Typography type="title" className={classes.headerBrandLogo}>
-              GPMS v1.3.0729
+              GPMS v1.3.0731
             </Typography>
             <IconButton onClick={this.props.toggleDrawer}>
               <MenuIcon />
